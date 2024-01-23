62fbcab92f948cc5e4a1e51c00ddcada862728b3
Teravista <chudy.robert660@gmail.com>
2024-01-23 17:07:25 +0100

    SCRUM-21-Unit-UI-added
    added healthbar, attack and name in small UI above every unit

--- Assets/Scripts/Entity/UnitController.cs
+++ Assets/Scripts/Entity/UnitController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitController : MonoBehaviour
 {
@@ -13,27 +15,64 @@ public class UnitController : MonoBehaviour
     public int attack;
     public int attackRange;
     public GameManager gameManager;
+    public GameObject unitUI;
 
     public void Init(PlayerManager playerManager, MapManager mapManager, GameManager gameManager) {
         this.owner = playerManager;
         this.mapManager = mapManager;
+        CreateUI();
         ApplyColor();
         unitMove.Init(mapManager);
         this.gameManager = gameManager;
         currentHealth = maxHealth;
     }
 
-    public void Activate() 
+    private void CreateUI()
+    {
+        GameObject myUI = Instantiate(unitUI, transform.position + new Vector3(0, 2, 0), Quaternion.identity, gameObject.transform);
+        myUI.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+
+        GameObject unitName = myUI.transform.Find("UnitName").gameObject;
+        TMP_Text nameText = unitName.GetComponent<TMP_Text>();
+        nameText.text = unitType.ToString();
+
+        GameObject unitAttack = myUI.transform.Find("AttackValue").gameObject;
+        TMP_Text attackText = unitAttack.GetComponent<TMP_Text>();
+        attackText.text = attack.ToString();
+
+        GameObject hpMeter = myUI.transform.Find("HpMeter").gameObject;
+        Image hpMeterValue = hpMeter.GetComponent<Image>();
+        float value = 1;
+        hpMeterValue.fillAmount = value;
+    }
+
+    private void UpdateUnitUI()
+    {
+        GameObject myUI = gameObject.transform.Find("UnitDefaultBar(Clone)").gameObject;
+
+        GameObject unitAttack = myUI.transform.Find("AttackValue").gameObject;
+        TMP_Text attackText = unitAttack.GetComponent<TMP_Text>();
+        attackText.text = attack.ToString();
+
+        GameObject hpMeter = myUI.transform.Find("HpMeter").gameObject;
+        Image hpMeterValue = hpMeter.GetComponent<Image>();
+        float value = (float)currentHealth/(float)maxHealth;
+        hpMeterValue.fillAmount = value;
+    }
+
+    public void Activate()
     {
         unitMove.Activate();
+        UpdateUnitUI();
         this.gameManager.setUnitTypeText(unitType.ToString());
-
+        this.gameManager.setUnitAttackText(attack.ToString());
     }
 
     public void Deactivate() 
     {
         unitMove.Deactivate();
         this.gameManager.setUnitTypeText("");
+        this.gameManager.setUnitAttackText("");
 
     }
 
--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -42,6 +42,7 @@ public class GameManager : MonoBehaviour
     private const int amountOfUnitTypes = 7;
     public GameObject[] unitPrefabs = new GameObject[amountOfUnitTypes];
     public GameObject unitTypeText;
+    public GameObject unitAttackText;
 
     void Start()
     {
@@ -120,6 +121,10 @@ public class GameManager : MonoBehaviour
 
     public void setUnitTypeText(string unitType) {
         unitTypeText.GetComponent<TMPro.TextMeshProUGUI>().text = unitType;
+    }
+    public void setUnitAttackText(string unitAttack)
+    {
+        unitTypeText.GetComponent<TMPro.TextMeshProUGUI>().text = unitAttack;
     }
 
     public GameObject getUnitPrefabByName(String unitType) {
