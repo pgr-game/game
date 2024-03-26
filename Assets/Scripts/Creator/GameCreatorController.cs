e321a2c17220353e3b7bd8f23413b8077a956eb3
iben0402 <81482747+iben0402@users.noreply.github.com>
2024-03-26 19:18:55 +0100

    add color selectors and change way of saving game settings

--- Assets/Scripts/Creator/GameCreatorController.cs
+++ Assets/Scripts/Creator/GameCreatorController.cs
@@ -13,49 +13,132 @@ public class GameCreatorController : MonoBehaviour
     public Slider numberOfPlayersSlider;
     public TMP_Text numberOfPlayersText;
     public TMP_Dropdown difficultyDropdown;
+    public TMP_Dropdown[] colorDropdowns = new TMP_Dropdown[4];
+    public TMP_Text colorDuplicateText;
     void Start()
     {
         gameSettings = GameObject.Find("GameSettings").GetComponent<GameSettings>();
-        
-        mapDropdown.onValueChanged.AddListener(delegate {
-            MapNameDropdownValueChanged(mapDropdown);
-        });
 
         numberOfPlayersSlider.onValueChanged.AddListener(delegate {
             NumberOfPlayersSliderValueChanged(numberOfPlayersSlider);
         });
-
-        difficultyDropdown.onValueChanged.AddListener(delegate {
-            DifficultyLevelDropdownValueChanged(difficultyDropdown);
-        });
     }
 
-    // Setting the map name in the GameSettings object
-    void MapNameDropdownValueChanged(TMP_Dropdown change)
-    {
-        gameSettings.GetComponent<GameSettings>().SetMapName(change.options[change.value].text);
-    }
-
-    // Setting the number of players in the GameSettings object
     void NumberOfPlayersSliderValueChanged(Slider change)
     {
-        gameSettings.GetComponent<GameSettings>().numberOfPlayers = (int)change.value;
         numberOfPlayersText.text = change.value.ToString();
+        setColorSelectorsVisibility((int)change.value);
     }
 
-    void DifficultyLevelDropdownValueChanged(TMP_Dropdown change)
+    public void setColorSelectorsVisibility(int numberOfPlayers)
     {
-        gameSettings.GetComponent<GameSettings>().difficulty = change.options[change.value].text;
+        for(int i=0; i<numberOfPlayers; i++) {
+            colorDropdowns[i].gameObject.SetActive(true);
+        }
+        for(int i=numberOfPlayers; i<4; i++) {
+            colorDropdowns[i].gameObject.SetActive(false);
+        }
+    } 
+
+    Color GetColorOfPlayerFromDropdown(TMP_Dropdown change)
+    {
+        var colorName = change.options[change.value].text;
+        Color color;
+
+        switch (colorName.ToLower())
+        {
+            case "red":
+                color = Color.red;
+                break;
+            case "blue":
+                color = Color.blue;
+                break;
+            case "green":
+                color = Color.green;
+                break;
+            case "magenta":
+                color = Color.magenta;
+                break;
+            case "cyan":
+                color = Color.cyan;
+                break;
+            case "yellow":
+                color = Color.yellow;
+                break;
+            default:
+                Debug.LogWarning($"Nieznany kolor: {colorName}");
+                color = Color.white;
+                break;
+        }
+        return color;
     }
 
-    // This method is called when the form is loaded
-    public void OnFormLoad() {
-        if(gameSettings==null) {
-            Start();
+    public void SetupGameSettings() {
+        // Map name
+        gameSettings.GetComponent<GameSettings>().SetMapName(mapDropdown.options[mapDropdown.value].text);
+
+        // Difficulty level
+        gameSettings.GetComponent<GameSettings>().difficulty = difficultyDropdown.options[difficultyDropdown.value].text;
+
+        // Number of players
+        int numberOfPlayers = (int)numberOfPlayersSlider.value;
+        gameSettings.GetComponent<GameSettings>().numberOfPlayers = numberOfPlayers;
+
+        // Player colors
+        gameSettings.playerColors = new Color[numberOfPlayers];
+        for(int i=0; i<numberOfPlayers; i++) {
+            gameSettings.playerColors[i] = GetColorOfPlayerFromDropdown(colorDropdowns[i]);
         }
-        gameSettings.mapName = "RiverDelta";
-        gameSettings.numberOfPlayers = 2;
-        gameSettings.difficulty = "Medium";
+
+        // todo player positions
+
+        // validation and starting game if valid
+        ValidateGameSettings();
+    }
+
+    public void ValidateGameSettings() {
+        Color[] colors = gameSettings.playerColors;
+        int[] duplicates = CheckForDuplicateColors(colors);
+        if(duplicates.Length == 0) {
+            this.gameObject.GetComponent<MainMenu>().StartGame();
+        }
+        else {
+            string message = "Colors for players: ";
+            foreach(int i in duplicates) {
+                message += (i+1) + ", ";
+            }
+            message = message.Substring(0, message.Length - 2);
+            message += " are duplicated! Please change before proceeding.";
+            colorDuplicateText.text = message;
+            colorDuplicateText.gameObject.SetActive(true);
+            //Debug.Log(message);
+        }
+    }
+
+    int[] CheckForDuplicateColors(Color[] colors)
+    {
+        Dictionary<Color, List<int>> colorIndices = new Dictionary<Color, List<int>>();
+        List<int> duplicates = new List<int>();
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            Color color = colors[i];
+            if (colorIndices.ContainsKey(color))
+            {
+                if (colorIndices[color].Count == 1)
+                {
+                    duplicates.Add(colorIndices[color][0]);
+                }
+                duplicates.Add(i); 
+                colorIndices[color].Add(i);
+            }
+            else
+            {
+                colorIndices[color] = new List<int> { i };
+            }
+        }
+
+        return duplicates.ToArray();
     }
 
 
